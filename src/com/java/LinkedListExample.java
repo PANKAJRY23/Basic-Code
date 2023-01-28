13b209c38dd52b07af0ad181bf12b1a3a5a443a4
Pankaj <pankajryadav23@gmail.com>
2023-01-29 01:21:26 +0530

    LinkedListed basics and factorial of a number programs'

---
+++ src/com/java/LinkedListExample.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		linkedList.add(2);
+		linkedList.addLast(1);
+		System.out.println(linkedList);
+	}
+
+}
