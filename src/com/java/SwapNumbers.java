e999a717d9092f1a2499b434dbcb2f9558f79845
Pankaj <pankajryadav23@gmail.com>
2023-02-02 02:30:40 +0530

    Swap two numbers

---
+++ src/com/java/SwapNumbers.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 20;
+		int temp = n1;
+		n1=n2;
+		n2=temp;
+		System.out.println(n1+" "+n2);
+	}
+
+}
